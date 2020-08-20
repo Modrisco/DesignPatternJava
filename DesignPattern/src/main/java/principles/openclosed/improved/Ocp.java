da6110a47d171afd5ad3ddf3addb3a074a80e277
Modrisco <menglingxu95@gmail.com>
2020-08-21 00:06:47 +1000

    improve with applying open-closed principle

--- DesignPattern/src/main/java/principles/openclosed/improved/Ocp.java
+++ DesignPattern/src/main/java/principles/openclosed/improved/Ocp.java
@@ -4,4 +4,70 @@ package principles.openclosed.improved;
  * @author mlx
  */
 public class Ocp {
+
+    public static void main(String[] args) {
+        GraphicEditor editor = new GraphicEditor();
+        editor.drawShape(new Rectangle());
+        editor.drawShape(new Circle());
+        editor.drawShape(new Triangle());
+        editor.drawShape(new Star());
+    }
+
+}
+
+class GraphicEditor {
+
+    public void drawShape(Shape s) {
+        s.draw();
+    }
+}
+
+abstract class Shape {
+    int m_type;
+
+    public abstract void draw();
+}
+
+class Rectangle extends Shape {
+    public Rectangle() {
+        super.m_type = 1;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("This is a rectangle");
+    }
+}
+
+class Circle extends Shape {
+    public Circle() {
+        super.m_type = 2;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("This is a circle");
+    }
+}
+
+class Triangle extends Shape {
+    public Triangle() {
+        super.m_type = 3;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("This is a triangle");
+    }
+}
+
+class Star extends Shape {
+    public Star() {
+        super.m_type = 4;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("This is a star");
+    }
 }
